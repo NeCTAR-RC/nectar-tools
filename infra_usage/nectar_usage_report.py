458d6dfc03804aa8bea99b21646eb24a2f0dbee6
Russell Sim <russell.sim@gmail.com>
2013-10-08 14:13:40 +1100

    Tried to reduce the amount of branching in main
    Change-Id: Ib9139ac74f49e8433d4743830c933f5c4d89c422

--- infra_usage/nectar_usage_report.py
+++ infra_usage/nectar_usage_report.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
-import sys
 import argparse
 
 from util_report import templateLoader, multiCSVNode
 from util_report import createCSVFileCloud, emailUser
 from util_report import createCSVFileNode, processConfig
 from util_nova import createNovaConnection
-from process_report import runCollect, combineResource, printOptions
-
-
-def getArgs():
-    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    parser.add_argument('-a', nargs='?', dest='t', action='store',
-                        required=True,
-                        help='{specify 1 cell name to overide,e,g -a np}')
-    parser.add_argument('-o', nargs='?', dest='o', default='n',
-                        choices=['html', 'csv', 'both', 'email'],
-                        required=False,
-                        help='output, {default: console}, {email: email html }'
-                        )
-    return parser.parse_args()
+from process_report import collect_all, combineResource, printOptions
 
 
 def main():
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    parser.add_argument('-a', nargs='?', dest='target_cell', action='store',
+                        required=True, default=True,
+                        help='{specify 1 cell name to overide,e,g -a np}')
+    parser.add_argument('-o', nargs='?', dest='output', default='n',
+                        choices=['html', 'csv', 'both', 'email'],
+                        required=False,
+                        help='output, {default: console}, {email: email html }')
+
+    opts = parser.parse_args()
+
     username = processConfig('production', 'user')
     key = processConfig('production', 'passwd')
     tenant_name = processConfig('production', 'name')
@@ -30,54 +27,43 @@ def main():
     zone = processConfig('config', 'zone')
     client = createNovaConnection(username, key, tenant_name, url)
     az = processConfig('config', 'az')
-    opt_ = getArgs()
-
-    if opt_.t is None:
-        data = runCollect(client, zone, opt=True)
-        data2 = combineResource(data)
-
-        if opt_.o is 'n':
-            printOptions(data, data_2=data2, options='all')
-
-        elif opt_.o == 'html':
-            templateLoader(data, data2)
-
-        elif opt_.o == 'csv':
-            multiCSVNode(data)
-            createCSVFileCloud(data2)
-
-        elif opt_.o == 'both':
-            templateLoader(data, data2)
-            multiCSVNode(data)
-            createCSVFileCloud(data2)
-
-        elif opt_.o == 'email':
-            file_l = templateLoader(data, data2, opt='email')
-            emailUser(file_l)
-
-    elif opt_.t in az:
-        data = runCollect(client, zone, opt=opt_.t)
-        if opt_.o is 'n':
-            printOptions(data)
-
-        elif opt_.o == 'html':
-            templateLoader(data, cell=opt_.t)
-
-        elif opt_.o == 'csv':
-            createCSVFileNode(data)
-
-        elif opt_.o == 'both':
-            templateLoader(data, cell=opt_.t)
-            createCSVFileNode(data)
-
-        elif opt_.o == 'email':
-            file_l = templateLoader(data, cell=opt_.t, opt='email')
-            emailUser(file_l)
-
+    if opts.target_cell is not None:
+        if opts.target_cell not in az:
+            parser.error("Error!, cell %s not found. Current cell %s " %
+                         (opts.target_cell, az))
     else:
-        print "Error!, cell %s not found. Current cell %s " % (opt_.t,
-                                                               az)
-        return sys.exit(1)
+        opts.target_cell = True
+
+    data = collect_all(client, zone, opt=opts.target_cell)
+    if opts.target_cell is True:
+        data2 = combineResource(data)
+    else:
+        data2 = None
+
+    if opts.output is 'n':
+        printOptions(data, data_2=data2, options='all')
+
+    elif opts.output == 'html':
+        templateLoader(data, data2)
+
+    elif opts.output == 'csv':
+        if opts.target_cell is True:
+            multiCSVNode(data)
+            createCSVFileCloud(data2)
+        else:
+            createCSVFileNode(data)
+
+    elif opts.output == 'both':
+        templateLoader(data, data2, cell=opts.target_cell)
+        if opts.target_cell is True:
+            multiCSVNode(data)
+            createCSVFileCloud(data2)
+        else:
+            createCSVFileNode(data)
+
+    elif opts.output == 'email':
+        file_l = templateLoader(data, data2, cell=opts.target_cell, opt='email')
+        emailUser(file_l)
 
 
 if __name__ == '__main__':
