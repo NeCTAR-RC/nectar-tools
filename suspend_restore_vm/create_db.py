dd2168b76b2e2927df105a86f72c67f23e0da366
Devendran Jagadisan <devendran.jagadisan@gmail.com>
2013-12-18 18:16:28 +1100

    script to suspend and restore vm
    Change-Id: I6e462b6eeb8320a9118c9c05b9a0cc69ff82a985

---
+++ suspend_restore_vm/create_db.py
@@ -0,0 +1,20 @@
+# table_def.py
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///vm_backup.db', echo=False)
+Base = declarative_base()
+
+
+class VM(Base):
+
+    __tablename__ = "vm_data"
+    id = Column(Integer, primary_key=True)
+    host = Column(String(10), nullable=False)
+    uuid = Column(String(256), nullable=False)
+    original_state = Column(String(20), nullable=False)
+    current_state = Column(String(20), nullable=False)
+    task_state = Column(String(20), nullable=False)
+
+Base.metadata.create_all(engine)
