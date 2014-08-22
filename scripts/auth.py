26f6dabfb5c5da1ad6603fc9ab16e8039cfed930
Kieran Spear <kispear@gmail.com>
2014-08-22 10:56:54 +1000

    Support OS_CACERT in clients
    Change-Id: I9897462bc82b412cb80acdb42ab1288f7f5ba998

--- scripts/auth.py
+++ scripts/auth.py
@@ -12,15 +12,17 @@ auth_username = os.environ.get('OS_USERNAME')
 auth_password = os.environ.get('OS_PASSWORD')
 auth_tenant = os.environ.get('OS_TENANT_NAME')
 auth_url = os.environ.get('OS_AUTH_URL')
+auth_cacert = os.environ.get('OS_CACERT')
 
 
 def get_keystone_client():
 
     try:
         kc = ksclient.Client(username=auth_username,
-                            password=auth_password,
-                            tenant_name=auth_tenant,
-                            auth_url=auth_url)
+                             password=auth_password,
+                             tenant_name=auth_tenant,
+                             auth_url=auth_url,
+                             cacert=auth_cacert)
     except AuthorizationFailure as e:
         print e
         print 'Have you sourced your openrc?'
@@ -31,23 +33,25 @@ def get_keystone_client():
 def get_nova_client():
 
     nc = novaclient.Client(auth_username,
-                        auth_password,
-                        auth_tenant,
-                        auth_url,
-                        service_type="compute")
+                           auth_password,
+                           auth_tenant,
+                           auth_url,
+                           cacert=auth_cacert,
+                           service_type="compute")
     return nc
 
 
 def get_cinder_client():
 
     cc = cinderclient.Client('1', auth_username,
-                            auth_password,
-                            auth_tenant,
-                            auth_url)
+                             auth_password,
+                             auth_tenant,
+                             auth_url,
+                             cacert=auth_cacert)
     return cc
 
 
 def get_glance_client(glance_url, token):
-    
-    gc = glanceclient.Client('1', glance_url, token=token)
+
+    gc = glanceclient.Client('1', glance_url, token=token, cacert=auth_cacert)
     return gc
