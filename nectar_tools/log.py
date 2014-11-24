ab410af44a501719cdceae91a581a25945d6e9ce
Kieran Spear <kispear@gmail.com>
2014-11-24 15:09:24 +1100

    Fix logging for update_expiry.py
    Change-Id: If37209432020688f5e9e4acad7701f3f8d243615

---
+++ nectar_tools/log.py
@@ -0,0 +1,68 @@
+import logging.config
+from os import path
+
+from nectar_tools.config import configurable, CONFIG
+
+
+@configurable('logging')
+def setup(filename=None, file_level='INFO', console_level='INFO',
+          enabled_loggers=None, log_format=None, log_dir=None):
+    if log_format is None:
+        log_format = '%(asctime)s %(name)s %(levelname)s %(message)s'
+    if enabled_loggers is None:
+        enabled_loggers = ['nectar_tools']
+    if isinstance(enabled_loggers, basestring):
+        enabled_loggers = enabled_loggers.split(',')
+
+    if CONFIG.args.debug:
+        console_level = 'DEBUG'
+        file_level = 'DEBUG'
+    if CONFIG.args.quiet:
+        console_level = None
+
+    config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+                'format': log_format,
+            },
+        },
+        'handlers': {
+            'null': {
+                'class': 'logging.NullHandler',
+            },
+            'console': {
+                'level': console_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'file': {
+                'level': file_level,
+                'class': 'logging.FileHandler',
+                'formatter': 'simple',
+                'filename': filename,
+            },
+        },
+    }
+    handlers = ['console', 'file', 'null']
+    if log_dir and filename:
+        config['handlers']['file']['filename'] = path.join(log_dir, filename)
+    else:
+        del config['handlers']['file']
+        handlers.remove('file')
+
+    # Disable console logging if it's not used.
+    if not console_level:
+        del config['handlers']['console']
+        handlers.remove('console')
+
+    config['loggers'] = {}
+    for module in enabled_loggers:
+        config['loggers'][module] = {
+            'handlers': handlers,
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+
+    logging.config.dictConfig(config)
