080a5664061deacbfc04c7238e0a19575b975201
Sam Morrison <sorrison@gmail.com>
2017-04-06 11:09:35 +1000

    Project Expiry System
    Handles Allocations and Project Trials
    
    Change-Id: I0c4a7200808f9f792d2543c1e83a34d14fb93df2

---
+++ nectar_tools/expiry/expiry_states.py
@@ -0,0 +1,33 @@
+ACTIVE = 'active'
+
+WARNING = 'warning'
+
+RESTRICTED = 'restricted'
+
+STOPPED = 'stopped'
+
+ARCHIVING = 'archiving'
+
+ARCHIVED = 'archived'
+
+DELETED = 'deleted'
+
+ARCHIVE_ERROR = 'archive error'
+
+RENEWED = 'renewed'
+
+ADMIN = 'admin'
+
+
+# Deprecated States
+QUOTA_WARNING = 'quota warning'
+
+PENDING_SUSPENSION = 'pending suspension'
+
+SUSPENDED = 'suspended'
+
+DEPRECATED_STATE_MAP = {
+    PENDING_SUSPENSION: RESTRICTED,
+    QUOTA_WARNING: WARNING,
+    SUSPENDED: STOPPED,
+}
