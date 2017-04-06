c39ec9797a4faddf9d22fbddcad22fc666d4d93f
Sam Morrison <sorrison@gmail.com>
2017-04-06 11:09:35 +1000

    Project Expiry System
    Handles Allocations and Project Trials
    
    Change-Id: I0c4a7200808f9f792d2543c1e83a34d14fb93df2

---
+++ nectar_tools/expiry/cmd/allocation_expirer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from nectar_tools.expiry.cmd import base
+from nectar_tools.expiry import expirer
+
+
+class AllocationManager(base.Manager):
+
+    @staticmethod
+    def valid_project(project):
+        return not expirer.PT_RE.match(project.name)
+
+    def add_args(self):
+        """Handle command-line options"""
+        super(AllocationManager, self).add_args()
+        self.parser.add_argument('--ignore-no-allocation', action='store_true',
+                        default=False,
+                        help='Ignore the fact that no allocation exists'),
+
+    def get_expirer(self, project):
+        return expirer.AllocationExpirer(
+            project=project,
+            ks_session=self.session,
+            dry_run=self.dry_run,
+            disable_project=self.args.disable_project,
+            force_no_allocation=self.args.ignore_no_allocation,
+            force_delete=self.args.force_delete)
+
+
+def main():
+    manager = AllocationManager()
+    if manager.args.status:
+        manager.print_status()
+        return
+
+    if manager.args.set_admin:
+        manager.set_admin()
+        return
+
+    manager.process_projects()
+
+
+if __name__ == '__main__':
+    main()
---
+++ nectar_tools/tests/fakes.py
@@ -0,0 +1,106 @@
+from nectar_tools.expiry import exceptions
+
+
+ALLOCATIONS = {
+    'dummy': {'id': 1,
+              'status': 'A',
+              'start_date': '2015-01-01',
+              'end_date': '2016-01-01',
+              'modified_time': '2015-01-02T10:10:10Z',
+              'contact_email': 'fake@fake.org'},
+    'warning1': {'id': 2,
+                 'status': 'A',
+                 'start_date': '2015-01-01',
+                 'end_date': '2017-01-01',
+                 'modified_time': '2015-01-02T10:10:10Z',
+                 'contact_email': 'fake@fake.org'},
+    'warning2': {'id': 3,
+                 'status': 'A',
+                 'start_date': '2016-12-15',
+                 'end_date': '2017-01-05',
+                 'modified_time': '2016-01-02T10:10:10Z',
+                 'contact_email': 'fake@fake.org'},
+    'active': {'id': 4,
+               'status': 'A',
+               'start_date': '2015-01-01',
+               'end_date': '2018-01-01',
+               'modified_time': '2015-01-02T10:10:10Z',
+               'contact_email': 'fake@fake.org'},
+    'pending1': {'id': 6,
+                 'status': 'X',
+                 'start_date': '2016-01-01',
+                 'end_date': '2016-07-01',
+                 'modified_time': '2016-01-02T10:10:10Z',
+                 'contact_email': 'fake@fake.org'},
+    'pending2': {'id': 7,
+                 'status': 'J',
+                 'start_date': '2016-01-01',
+                 'end_date': '2017-07-01',
+                 'modified_time': '2016-12-02T10:10:10Z',
+                 'contact_email': 'fake@fake.org'},
+    'expired': {'id': 10,
+                'status': 'A',
+                'start_date': '2015-01-01',
+                'end_date': '2016-07-01',
+                'modified_time': '2015-01-02T10:10:10Z',
+                'contact_email': 'fake@fake.org'},
+}
+
+
+class FakeProject(object):
+
+    def __init__(self, project_id='dummy', name='MyProject', **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        self.id = project_id
+        self.name = name
+
+
+class FakeAllocationSession(object):
+
+    def __init__(self, allocations):
+        self.allocations = ALLOCATIONS
+
+    def get_current_allocation(self, project_id='dummy'):
+        try:
+            return self.allocations[project_id]
+        except KeyError:
+            raise exceptions.AllocationDoesNotExist(project_id=project_id)
+
+
+class FakeInstance(object):
+
+    def __init__(self, id='fake', status='ACTIVE', metadata={},
+                 task_state='', vm_state='ACTIVE', host='fakehost'):
+        self.id = id
+        self.status = status
+        self.metadata = metadata
+        setattr(self, 'OS-EXT-STS:task_state', task_state)
+        setattr(self, 'OS-EXT-STS:vm_state', vm_state)
+        setattr(self, 'OS-EXT-SRV-ATTR:host', host)
+
+
+class FakeImage(object):
+
+    def __init__(self, id='fake', name='fake_archive', status='active',
+                 **kwargs):
+        self.id = id
+        self.name = name
+        self.status = status
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+
+
+class FakeVolume(object):
+
+    def __init__(self, id='fake',
+                 **kwargs):
+        self.id = id
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
