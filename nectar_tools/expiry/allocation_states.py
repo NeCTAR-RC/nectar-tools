64a34b32a6a0ce9e781c101d96ae601946b2dddb
Sam Morrison <sorrison@gmail.com>
2017-04-06 11:09:35 +1000

    Project Expiry System
    Handles Allocations and Project Trials
    
    Change-Id: I0c4a7200808f9f792d2543c1e83a34d14fb93df2

---
+++ nectar_tools/expiry/allocation_states.py
@@ -0,0 +1,33 @@
+NEW = 'N'
+
+SUBMITTED = 'E'
+
+APPROVED = 'A'
+
+DECLINED = 'R'
+
+UPDATE_PENDING = 'X'
+
+UPDATE_DECLINED = 'J'
+
+PROVISIONED = 'P'
+
+LEGACY = 'L'
+
+LEGACY_APPROVED = 'M'
+
+LEGACY_REJECTED = 'O'
+
+
+STATES = {
+    NEW: 'New',
+    SUBMITTED: 'Submitted',
+    APPROVED: 'Approved',
+    DECLINED: 'Declined',
+    UPDATE_PENDING: 'Update Pending',
+    UPDATE_DECLINED: 'Update Declined',
+    PROVISIONED: 'Provisioned',
+    LEGACY: 'Legacy',
+    LEGACY_APPROVED: 'Legacy Approved',
+    LEGACY_REJECTED: 'Legacy Rejected',
+}
---
+++ nectar_tools/expiry/exceptions.py
@@ -0,0 +1,21 @@
+
+class InvalidProject(Exception):
+    pass
+
+
+class InvalidProjectTrial(InvalidProject):
+    pass
+
+
+class InvalidProjectAllocation(InvalidProject):
+    pass
+
+
+class AllocationDoesNotExist(Exception):
+
+    def __init__(self, project_id):
+        self.project_id = project_id
+
+
+class NoUsageError(Exception):
+    pass
