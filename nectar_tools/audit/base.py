1ceaf12705d1a57672f4942fd53b415c22b7f2c8
Sam Morrison <sorrison@gmail.com>
2019-03-19 14:06:48 +1100

    Add a framework for doing auditing on openstack resources
    Adds some initial audit checks for Allocations and PT's
    
    Change-Id: Ie49284796d9a14f4f0a1dc4e6fd9933cbf707ade

---
+++ nectar_tools/audit/base.py
@@ -0,0 +1,10 @@
+
+class Auditor(object):
+
+    def run_all(self):
+        public_method_names = [method for method in dir(self)
+                               if callable(getattr(self, method))
+                               if not method.startswith('_')]
+        for method in public_method_names:
+            if method != 'run_all':
+                getattr(self, method)()
