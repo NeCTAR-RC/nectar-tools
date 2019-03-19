6992a4b5f880ad13157207bc871e014a3567d070
Sam Morrison <sorrison@gmail.com>
2019-03-19 14:06:48 +1100

    Add a framework for doing auditing on openstack resources
    Adds some initial audit checks for Allocations and PT's
    
    Change-Id: Ie49284796d9a14f4f0a1dc4e6fd9933cbf707ade

---
+++ nectar_tools/audit/cmd/project_base.py
@@ -0,0 +1,33 @@
+import sys
+
+from nectar_tools import cmd_base
+
+
+class ProjectAuditor(cmd_base.CmdBase):
+
+    def add_args(self):
+        super(ProjectAuditor, self).add_args()
+        self.parser.description = 'Project auditor'
+        project_group = self.parser.add_mutually_exclusive_group()
+        project_group.add_argument('--all', action='store_true',
+                            help='Run over all projects')
+        project_group.add_argument('-p', '--project-id',
+                            help='Project ID to process')
+
+    def run_audits(self):
+        manager = self.get_manager()
+        projects = []
+        if self.args.project_id:
+            project = self.k_client.projects.get(self.args.project_id)
+            projects.append(project)
+        elif self.args.all:
+            projects = self.k_client.projects.list(enabled=True)
+            projects.sort(key=lambda p: p.name.split('-')[-1].zfill(5))
+        else:
+            print("Must specify either project_id or all argument")
+            return sys.exit(1)
+
+        for project in projects:
+            if self.is_valid_project(project):
+                auditor = manager(ks_session=self.session, project=project)
+                auditor.run_all()
