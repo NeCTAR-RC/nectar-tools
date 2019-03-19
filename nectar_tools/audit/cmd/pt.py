c5add8e4b02a5dc8ce1c4a87bc9d5bad583dbb44
Sam Morrison <sorrison@gmail.com>
2019-03-19 14:06:48 +1100

    Add a framework for doing auditing on openstack resources
    Adds some initial audit checks for Allocations and PT's
    
    Change-Id: Ie49284796d9a14f4f0a1dc4e6fd9933cbf707ade

---
+++ nectar_tools/audit/cmd/pt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from nectar_tools.audit.cmd import project_base
+from nectar_tools.audit.projects import pt
+from nectar_tools import utils
+
+
+class PTAuditor(project_base.ProjectAuditor):
+
+    @staticmethod
+    def get_manager():
+        return pt.ProjectTrialAuditor
+
+    def is_valid_project(self, project):
+        return utils.valid_project_trial(project)
+
+
+def main():
+    cmd = PTAuditor()
+    cmd.run_audits()
+
+
+if __name__ == '__main__':
+    main()
