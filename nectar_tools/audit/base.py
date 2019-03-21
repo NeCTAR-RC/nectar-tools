aa2c8266eb323625db324ebfb38162cda5a072f5
Sam Morrison <sorrison@gmail.com>
2019-03-22 09:46:12 +1100

    Add audit checks for gnocchi instances
    Change-Id: I58b4aa8768331278c0ab92b7b48ce787a42c02ae

--- nectar_tools/audit/base.py
+++ nectar_tools/audit/base.py
@@ -1,3 +1,8 @@
+import logging
+
+
+LOG = logging.getLogger(__name__)
+
 
 class Auditor(object):
 
@@ -7,4 +12,6 @@ class Auditor(object):
                                if not method.startswith('_')]
         for method in public_method_names:
             if method != 'run_all':
+                LOG.debug("Starting %s", method)
                 getattr(self, method)()
+                LOG.debug("Finished %s", method)
