6bb697b0b2ba26ba946f0d81ad5b913f946f74e0
Sam Morrison <sorrison@gmail.com>
2019-04-03 15:54:15 +1100

    New auditor to find unused roles
    Change-Id: If5f4825038d849b2933187cbeade84b2886d7a57

---
+++ nectar_tools/audit/cmd/identity.py
@@ -0,0 +1,22 @@
+from nectar_tools.audit.identity import role
+from nectar_tools import cmd_base
+
+
+class IdentityAuditorCmd(cmd_base.CmdBase):
+
+    def add_args(self):
+        super(IdentityAuditorCmd, self).add_args()
+        self.parser.description = 'Identity auditor'
+
+    def run_audits(self):
+        role_auditor = role.RoleAuditor(self.session)
+        role_auditor.run_all()
+
+
+def main():
+    cmd = IdentityAuditorCmd()
+    cmd.run_audits()
+
+
+if __name__ == '__main__':
+    main()
