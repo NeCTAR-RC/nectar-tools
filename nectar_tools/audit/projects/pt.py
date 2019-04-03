9959b7255147ebcda3e04cede37ca51b8a572698
Sam Morrison <sorrison@gmail.com>
2019-04-03 13:56:20 +1100

    Only look at member role when auditing project trial members
    Change-Id: Ic21955bee78343a435847df76e48443afe0bbd8e

--- nectar_tools/audit/projects/pt.py
+++ nectar_tools/audit/projects/pt.py
@@ -1,15 +1,19 @@
 import logging
 
 from nectar_tools.audit.projects import base
+from nectar_tools import config
 
 
+CONF = config.CONFIG
 LOG = logging.getLogger(__name__)
 
 
 class ProjectTrialAuditor(base.ProjectAuditor):
 
     def check_owner(self):
-        assignments = self.k_client.role_assignments.list(project=self.project)
+        assignments = self.k_client.role_assignments.list(
+            project=self.project,
+            role=CONF.keystone.member_role_id)
         if len(assignments) > 1:
             LOG.info("%s: More than 1 user", self.project.id)
         if len(assignments) < 1:
