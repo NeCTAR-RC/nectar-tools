c3e570618d3153618e40d6238a1f253274ab1a71
Sam Morrison <sorrison@gmail.com>
2019-04-08 09:49:28 +1000

    Add -l option to auditors to just list audits
    Also refactor names to make it less confussing
    
    Change-Id: I1ef4327439c73e558c1945343a18ad0e9536ed6a

--- nectar_tools/audit/base.py
+++ nectar_tools/audit/base.py
@@ -6,12 +6,18 @@ LOG = logging.getLogger(__name__)
 
 class Auditor(object):
 
-    def run_all(self):
+    def run_all(self, list_not_run=False):
         public_method_names = [method for method in dir(self)
                                if callable(getattr(self, method))
                                if not method.startswith('_')]
         for method in public_method_names:
             if method != 'run_all':
+                if list_not_run:
+                    path = "%s:%s.%s" % (type(self).__module__,
+                                         type(self).__name__,
+                                         method)
+                    print(path)
+                    continue
                 LOG.debug("Starting %s", method)
                 try:
                     getattr(self, method)()
--- nectar_tools/audit/cmd/metric.py
+++ nectar_tools/audit/cmd/metric.py
@@ -1,24 +1,20 @@
 from nectar_tools.audit.metric import instance
 from nectar_tools.audit.metric import resource_provider
-from nectar_tools import cmd_base
+from nectar_tools.audit.cmd import base
 
 
-class MetricAuditor(cmd_base.CmdBase):
+class MetricAuditorCmd(base.AuditCmdBase):
+
+    AUDITORS = [instance.InstanceAuditor,
+                resource_provider.ResourceProviderAuditor]
 
     def add_args(self):
-        super(MetricAuditor, self).add_args()
+        super(MetricAuditorCmd, self).add_args()
         self.parser.description = 'Metric auditor'
-
-    def run_audits(self):
-        instance_auditor = instance.InstanceAuditor(ks_session=self.session)
-        instance_auditor.run_all()
-        rp_auditor = resource_provider.ResourceProviderAuditor(
-            ks_session=self.session)
-        rp_auditor.run_all()
 
 
 def main():
-    cmd = MetricAuditor()
+    cmd = MetricAuditorCmd()
     cmd.run_audits()
 
 
