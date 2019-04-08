a08243baf41dea84ff5144cabf98094baa9100e2
Sam Morrison <sorrison@gmail.com>
2019-04-08 13:40:15 +1000

    Add ability to just run a specific audit check
    Change-Id: Ic27117a5982d48392384386df5c28d67c0582344

--- nectar_tools/audit/cmd/base.py
+++ nectar_tools/audit/cmd/base.py
@@ -1,4 +1,11 @@
+import logging
+import importlib
+import sys
+
 from nectar_tools import cmd_base
+
+
+LOG = logging.getLogger(__name__)
 
 
 class AuditCmdBase(cmd_base.CmdBase):
@@ -10,6 +17,20 @@ class AuditCmdBase(cmd_base.CmdBase):
         if self.args.list:
             self.list_not_run = True
 
+        if self.args.check:
+            try:
+                module_str, class_method_str = self.args.check.split(':')
+                class_str, method_str = class_method_str.split('.')
+                module = importlib.import_module(module_str)
+                auditor_class = getattr(module, class_str)
+                auditor = auditor_class(ks_session=self.session)
+                method = getattr(auditor, method_str)
+                method()
+                sys.exit(0)
+            except Exception as e:
+                LOG.exception(e)
+                sys.exit(1)
+
     def run_audits(self):
         for auditor in self.AUDITORS:
             a = auditor(ks_session=self.session)
@@ -19,3 +40,5 @@ class AuditCmdBase(cmd_base.CmdBase):
         super(AuditCmdBase, self).add_args()
         self.parser.add_argument('-l', '--list', action='store_true',
                                  help="List audits but don't run them")
+        self.parser.add_argument('check', nargs='?',
+                                 help="specific check to run")
