f0b6e142f6b24d433ff2f44f24da2e31f5a650f1
Sam Morrison <sorrison@gmail.com>
2019-07-12 09:54:43 +1000

    Add auditor to find instances of deleted projects
    Change-Id: I397019d6dde38dc5c5a6b031fd9709b854899e1a

---
+++ nectar_tools/audit/identity/project.py
@@ -0,0 +1,27 @@
+import logging
+
+
+from nectar_tools.expiry import archiver
+from nectar_tools.audit.identity import base
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ProjectAuditor(base.IdentityAuditor):
+
+    def __init__(self, ks_session):
+        super(ProjectAuditor, self).__init__(ks_session=ks_session)
+        self.ks_session = ks_session
+        self.projects = self.k_client.projects.list()
+
+    def check_deleted_no_instances(self):
+        for project in self.projects:
+            status = getattr(project, 'expiry_status', None)
+            if status == 'deleted':
+                nova_archiver = archiver.NovaArchiver(
+                    {'project': project}, ks_session=self.ks_session)
+                instances = nova_archiver._all_instances()
+                if instances:
+                    LOG.error("%s has %s running instances",
+                              project.name, len(instances))
