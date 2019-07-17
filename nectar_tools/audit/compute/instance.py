1b90be751a66d6529fb1e3db021595216d27c8b7
Adrian Smith <aussieade@gmail.com>
2019-07-17 15:07:13 +1000

    report on instances stuck in wierd states for > 7 days
    note that adding in any of the commented out states seems
    to somehow include all 'ACTIVE' too - I hope someone can
    explain that behaviour ?
    
    Change-Id: Ieba8d1b01e10f4515405c185746e816b5cfac370

---
+++ nectar_tools/audit/compute/instance.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from datetime import timedelta
+import logging
+
+from nectar_tools.audit import base
+
+
+LOG = logging.getLogger(__name__)
+
+STATES = [
+    'ERROR',
+    'BUILD',
+    'UNKNOWN',
+    'VERIFY_RESIZE',
+    'REVERT_RESIZE',
+    'SUSPENDED',
+    'SHUTOFF',
+#    'REBUILD',
+#    'HARD_REBOOT',
+#    'MIGRATING',
+#    'REBOOT',
+    ]
+
+time_diff = timedelta(days=7)
+
+
+class InstanceAuditor(base.Auditor):
+
+    def __init__(self, ks_session):
+        super(InstanceAuditor, self).__init__(ks_session=ks_session)
+        self.instances = self.sdk_client.compute.servers(status=STATES)
+
+    def check_instance_states(self):
+        for instance in self.instances:
+            updated_at = datetime.strptime(instance.updated_at,
+                                           '%Y-%m-%dT%H:%M:%SZ')
+            now = datetime.now()
+            if updated_at < now - time_diff:
+                LOG.info("instance %s in state %s for more than %s",
+                         instance.id, instance.status, time_diff)
