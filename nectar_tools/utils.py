ffb6056253299ad42d0116ebc9e226cabe9b399c
Adrian Smith <aussieade@gmail.com>
2019-07-31 12:12:38 +1000

    report on dns zones stuck for more than 5 mins
    Change-Id: I6da31e88bbc5762b6669f8b7c4fbc249eccd3a04

--- nectar_tools/utils.py
+++ nectar_tools/utils.py
@@ -10,3 +10,13 @@ def valid_project_trial(project):
 
 def valid_project_allocation(project):
     return not valid_project_trial(project)
+
+
+def get_resources(api_call, mark_name, **kwargs):
+    results = api_call(kwargs)
+    while (True):
+        next = api_call(kwargs, marker=results[-1].get(mark_name))
+        if len(next) == 0:
+            break
+        results += next
+    return results
