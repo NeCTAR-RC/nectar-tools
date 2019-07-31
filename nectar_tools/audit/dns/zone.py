3e3e21db5ac623989992ea2b2afc3bcb2dcbb51b
Adrian Smith <aussieade@gmail.com>
2019-07-31 12:12:38 +1000

    report on dns zones stuck for more than 5 mins
    Change-Id: I6da31e88bbc5762b6669f8b7c4fbc249eccd3a04

---
+++ nectar_tools/audit/dns/zone.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from datetime import timedelta
+from designateclient.client import Client as designateclient
+import logging
+
+from nectar_tools.audit import base
+from nectar_tools.utils import get_resources
+
+
+LOG = logging.getLogger(__name__)
+
+STATES = ['ERROR', 'PENDING']
+
+time_diff = timedelta(minutes=5)
+
+
+class DnsAuditor(base.Auditor):
+
+    def __init__(self, ks_session):
+        super(DnsAuditor, self).__init__(ks_session=ks_session)
+        self.dc = designateclient('2', session=ks_session,
+                                  all_projects=True)
+        self.zones = get_resources(self.dc.zones.list, 'id')
+
+    def check_zone_states(self):
+        for zone in self.zones:
+            if zone['status'] in STATES:
+                updated_at = datetime.strptime(zone['updated_at'],
+                                               '%Y-%m-%dT%H:%M:%S.%f')
+                now = datetime.now()
+                if updated_at < now - time_diff:
+                    LOG.info("zone %s in state %s for more than %s",
+                             zone['id'], zone['status'], time_diff)
