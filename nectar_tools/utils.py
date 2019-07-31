bdfc3e85f5d80aedcff7b2242c7576116840eb23
Adrian Smith <aussieade@gmail.com>
2019-07-31 12:12:38 +1000

    report on dns zones stuck for more than 12 hours
    Change-Id: I6da31e88bbc5762b6669f8b7c4fbc249eccd3a04

--- nectar_tools/utils.py
+++ nectar_tools/utils.py
@@ -10,3 +10,21 @@ def valid_project_trial(project):
 
 def valid_project_allocation(project):
     return not valid_project_trial(project)
+
+
+def list_resources(list_method, marker_name='id', **list_method_args):
+    """get a list of all resources from an api call
+
+    :param func list_method: api call used to generate list
+    :param str marker_name: name of marker object in api_call
+    :param dict **list_method_args: list_method args to pass through
+    """
+    results = list_method(**list_method_args)
+    if results:
+        while (True):
+            next = list_method(**list_method_args,
+                               marker=results[-1].get(marker_name))
+            if len(next) == 0:
+                break
+            results += next
+    return results
