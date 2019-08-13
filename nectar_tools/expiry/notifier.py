d2fcff81ccc76fac6b60ba44d7db0cd76994e297
Rocky <shi.yan@unimelb.edu.au>
2019-08-13 11:12:41 +1000

    Change notifier to use generic resource
    In order to introduce the processes for image expiry and instance
    expiry later, we need to make the notifier related classes more generic.
    
    Change-Id: If0e81a2b8a16508d35c1a1577b88cc1dfc30c385

--- nectar_tools/expiry/notifier.py
+++ nectar_tools/expiry/notifier.py
@@ -1,3 +1,4 @@
+import jinja2
 import logging
 
 from nectar_tools import auth
@@ -11,11 +12,13 @@ LOG = logging.getLogger(__name__)
 
 class ExpiryNotifier(notifier.FreshDeskNotifier):
 
-    def __init__(self, project, template_dir, group_id, subject,
-                 ks_session=None, dry_run=False):
+    def __init__(self, resource_type, resource, template_dir, group_id,
+                 subject, ks_session=None, dry_run=False):
         super(ExpiryNotifier, self).__init__(
-            project, template_dir, group_id, subject, dry_run)
+            resource_type, resource, template_dir, group_id, subject, dry_run)
         self.k_client = auth.get_keystone_client(ks_session)
+        self.g_client = auth.get_glance_client(ks_session)
+        self.n_client = auth.get_nova_client(ks_session)
 
     def send_message(self, stage, owner, extra_context={},
                      extra_recipients=[]):
@@ -40,11 +43,15 @@ class ExpiryNotifier(notifier.FreshDeskNotifier):
                                             extra_context=extra_context,
                                             tags=['expiry'])
             self._set_ticket_id(ticket_id)
+            try:
+                details = self.render_template(
+                    '%s-details.tmpl' % self.resource_type, extra_context)
 
-            details = self.render_template(
-                'project-details.tmpl', extra_context)
+                self._add_note_to_ticket(ticket_id, details)
 
-            self._add_note_to_ticket(ticket_id, details)
+            except jinja2.TemplateNotFound:
+                LOG.info("Skip adding note for ticket %s, %s-details.tmpl \
+                         template is not found", ticket_id, self.resource_type)
 
     def finish(self, message=None):
         ticket_id = self._get_ticket_id()
@@ -54,22 +61,38 @@ class ExpiryNotifier(notifier.FreshDeskNotifier):
 
             if not self.dry_run:
                 # Status 4 == Resolved
-                LOG.info("%s: Resolving ticket %s", self.project.id, ticket_id)
+                LOG.info("%s: Resolving ticket %s", self.resource.id,
+                         ticket_id)
                 self.api.tickets.update_ticket(ticket_id, status=4)
             else:
-                LOG.info("%s: Would resolve ticket %s", self.project.id,
+                LOG.info("%s: Would resolve ticket %s", self.resource.id,
                          ticket_id)
 
     def _set_ticket_id(self, ticket_id):
         if not self.dry_run:
-            self.k_client.projects.update(self.project.id,
-                                          expiry_ticket_id=str(ticket_id))
-        msg = '%s: Setting expiry_ticket_id=%s' % (self.project.id,
+            if self.resource_type == 'project':
+                self.k_client.projects.update(self.resource.id,
+                    expiry_ticket_id=str(ticket_id))
+            elif self.resource_type == 'image':
+                self.g_client.images.update(self.resource.id,
+                    nectar_expiry_ticket_id=str(ticket_id))
+                # use "nectar_" prefix for image property protection
+            elif self.resource_type == 'instance':
+                self.n_client.servers.set_meta(self.resource.id,
+                    {'expiry_ticket_id': str(ticket_id)})
+        msg = '%s: Setting expiry_ticket_id=%s' % (self.resource.id,
                                                    ticket_id)
         LOG.debug(msg)
 
     def _get_ticket_id(self):
         try:
-            return int(getattr(self.project, 'expiry_ticket_id', 0))
+            if self.resource_type == 'project':
+                return int(getattr(self.resource, 'expiry_ticket_id', 0))
+            elif self.resource_type == 'image':
+                return int(getattr(self.resource,
+                                   'nectar_expiry_ticket_id', 0))
+            elif self.resource_type == 'instance':
+                return int(self.resource.metadata.get(
+                           'expiry_ticket_id', 0))
         except ValueError:
             return 0
