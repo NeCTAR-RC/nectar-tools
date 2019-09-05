138b7708b69818bc5a2ea856c3f95a489bd641a8
Stephen Crawley <s.crawley@uq.edu.au>
2019-09-05 10:58:43 +1000

    Project allocation audit enhancements:
    1. Refine the check on the allocation -> project backlink
    2. Check for inconsistencies between allocation and expiry states,
       and the project enabled flag.
    
    Change-Id: Ib69eb5066c4e0ece2403b261fad7e48e86d2f479

--- nectar_tools/audit/projects/allocation.py
+++ nectar_tools/audit/projects/allocation.py
@@ -1,9 +1,11 @@
 import logging
 
 from nectarallocationclient import exceptions as allocation_exceptions
+from nectarallocationclient import states as allocation_states
 
 from nectar_tools.audit.projects import base
 from nectar_tools import auth
+from nectar_tools.expiry import expiry_states
 
 
 LOG = logging.getLogger(__name__)
@@ -18,7 +20,7 @@ class ProjectAllocationAuditor(base.ProjectAuditor):
     def check_allocation_id(self):
         allocation_id = getattr(self.project, 'allocation_id', None)
         expiry_status = getattr(self.project, 'expiry_status', '')
-        if expiry_status == 'admin':
+        if expiry_status == expiry_states.ADMIN:
             return
         if not allocation_id:
             LOG.info("%s: No allocation_id", self.project.id)
@@ -45,6 +47,41 @@ class ProjectAllocationAuditor(base.ProjectAuditor):
             LOG.info("%s: Linked allocation_id not found", self.project.id)
             return
 
-        if allocation.project_id != self.project.id:
+        if allocation.project_id is None or allocation.project_id == '':
+            LOG.info("%s: Linked allocation not linked back to project",
+                     self.project.id)
+        elif allocation.project_id != self.project.id:
             LOG.info("%s: Linked allocation_id project mismatch",
                      self.project.id)
+
+    def check_deleted_allocation(self):
+        allocation_id = getattr(self.project, 'allocation_id', None)
+        expiry_status = getattr(self.project, 'expiry_status', '')
+        if not allocation_id:
+            return
+        try:
+            allocation = self.a_client.allocations.get(allocation_id)
+        except allocation_exceptions.NotFound:
+            # Reported by another check
+            return
+
+        if allocation.status == allocation_states.DELETED \
+           and expiry_status != expiry_states.DELETED:
+            LOG.info("%s: Live project linked to deleted allocation",
+                     self.project.id)
+        elif allocation.status != allocation_states.DELETED \
+             and expiry_status == expiry_states.DELETED:
+            LOG.info("%s: Deleted project linked to live allocation",
+                     self.project.id)
+
+        if self.project.enabled:
+            if expiry_status != expiry_states.DELETED:
+                LOG.info("%s: Expiry deleted project is not disabled",
+                         self.project.id)
+            elif allocation.status == allocation_states.DELETED:
+                LOG.info("%s: Project for deleted allocation is not disabled",
+                         self.project.id)
+        else:
+            if allocation.status != allocation_states.DELETED:
+                LOG.info("%s: Disabled project with active allocation",
+                         self.project.id)
