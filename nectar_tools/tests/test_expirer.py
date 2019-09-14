5ac3a38850695bc4bfbdfe62122819ca836cade9
Rocky <shi.yan@unimelb.edu.au>
2019-09-14 19:23:22 +1000

    Move finish_expiry to base class
    It could be easily used in other expirer subclass
    
    Change-Id: I27d8352c53abb691720072db7bec50c9a0e8039b

--- nectar_tools/tests/test_expirer.py
+++ nectar_tools/tests/test_expirer.py
@@ -664,7 +664,8 @@ class AllocationExpiryTests(test.TestCase):
             mock.patch.object(ex, 'finish_expiry'),
         ) as (mock_archiver, mock_finish):
             ex.revert_expiry()
-            mock_finish.assert_called_once_with()
+            mock_finish.assert_called_once_with(
+                message='Allocation has been renewed')
             mock_archiver.reset_quota.assert_not_called()
             mock_archiver.enable_resources.assert_called_once_with()
 
@@ -679,7 +680,8 @@ class AllocationExpiryTests(test.TestCase):
             mock.patch.object(ex, 'finish_expiry'),
         ) as (mock_archiver, mock_finish):
             ex.revert_expiry()
-            mock_finish.assert_called_once_with()
+            mock_finish.assert_called_once_with(
+                message='Allocation has been renewed')
             mock_archiver.reset_quota.assert_called_once_with()
             mock_archiver.enable_resources.assert_called_once_with()
 
@@ -694,7 +696,8 @@ class AllocationExpiryTests(test.TestCase):
             mock.patch.object(ex, 'finish_expiry'),
         ) as (mock_archiver, mock_finish):
             ex.revert_expiry()
-            mock_finish.assert_called_once_with()
+            mock_finish.assert_called_once_with(
+                message='Allocation has been renewed')
             mock_archiver.reset_quota.assert_called_once_with()
             mock_archiver.enable_resources.assert_called_once_with()
 
@@ -709,7 +712,8 @@ class AllocationExpiryTests(test.TestCase):
             mock.patch.object(ex, 'finish_expiry'),
         ) as (mock_archiver, mock_finish):
             ex.revert_expiry()
-            mock_finish.assert_called_once_with()
+            mock_finish.assert_called_once_with(
+                message='Allocation has been renewed')
             mock_archiver.reset_quota.assert_called_once_with()
             mock_archiver.enable_resources.assert_called_once_with()
 
