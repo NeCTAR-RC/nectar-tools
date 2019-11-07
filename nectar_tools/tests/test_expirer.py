03115c39091eaed47964be6f16682c837eccc476
Rocky <shi.yan@unimelb.edu.au>
2019-11-07 16:32:29 +1100

    Minor change to use Constant in expirer test
    Change-Id: I926c8f95b565e04f1bea30b7d9b6f40ec102b0b4

--- nectar_tools/tests/test_expirer.py
+++ nectar_tools/tests/test_expirer.py
@@ -1079,7 +1079,7 @@ class PTExpiryTests(test.TestCase):
             self.assertTrue(processed)
 
     def test_process_archived(self, mock_session):
-        project = FakeProjectWithOwner(expiry_status='archived',
+        project = FakeProjectWithOwner(expiry_status=expiry_states.ARCHIVED,
                                        expiry_next_step=BEFORE)
         ex = expirer.PTExpirer(project)
         with mock.patch.object(ex, 'delete_project') as mock_delete:
@@ -1088,7 +1088,7 @@ class PTExpiryTests(test.TestCase):
             self.assertTrue(processed)
 
     def test_process_archived_not_next_step(self, mock_session):
-        project = FakeProjectWithOwner(expiry_status='archived',
+        project = FakeProjectWithOwner(expiry_status=expiry_states.ARCHIVED,
                                        expiry_next_step=AFTER)
         ex = expirer.PTExpirer(project)
         with mock.patch.object(ex, 'delete_resources') as mock_delete:
@@ -1097,8 +1097,9 @@ class PTExpiryTests(test.TestCase):
             self.assertFalse(processed)
 
     def test_process_archive_error(self, mock_session):
-        project = FakeProjectWithOwner(expiry_status='archive error',
-                              expiry_next_step=BEFORE)
+        project = FakeProjectWithOwner(
+            expiry_status=expiry_states.ARCHIVE_ERROR,
+            expiry_next_step=BEFORE)
         ex = expirer.PTExpirer(project)
         with mock.patch.object(ex, 'delete_project') as mock_delete:
             processed = ex.process()
@@ -1106,8 +1107,8 @@ class PTExpiryTests(test.TestCase):
             self.assertTrue(processed)
 
     def test_process_suspended(self, mock_session):
-        project = FakeProjectWithOwner(expiry_status='suspended',
-                              expiry_next_step=BEFORE)
+        project = FakeProjectWithOwner(expiry_status=expiry_states.SUSPENDED,
+                                       expiry_next_step=BEFORE)
         ex = expirer.PTExpirer(project)
         with mock.patch.object(ex, 'archive_project') as mock_archive:
             processed = ex.process()
@@ -1179,7 +1180,7 @@ class PTExpiryTests(test.TestCase):
             next_step = datetime.datetime.now() + relativedelta(days=18)
             next_step = next_step.strftime(expirer.DATE_FORMAT)
             mock_update_resource.assert_called_with(
-                expiry_status='quota warning',
+                expiry_status=expiry_states.QUOTA_WARNING,
                 expiry_next_step=next_step)
 
     def test_notify_near_limit_notification_exception(self, mock_session):
@@ -1221,7 +1222,7 @@ class PTExpiryTests(test.TestCase):
             mock_notification.assert_called_with('second-warning')
             mock_event.assert_called_once_with('second-warning')
             mock_update_resource.assert_called_with(
-                expiry_status='pending suspension',
+                expiry_status=expiry_states.PENDING_SUSPENSION,
                 expiry_next_step=new_expiry)
             mock_archiver.zero_quota.assert_called_with()
 
@@ -1252,8 +1253,9 @@ class PTExpiryTests(test.TestCase):
                 mock_event.assert_not_called()
 
     def test_notify_over_limit(self, mock_session):
-        project = FakeProjectWithOwner(expiry_status='pending suspension',
-                              expiry_next_step='2014-01-01')
+        project = FakeProjectWithOwner(
+            expiry_status=expiry_states.PENDING_SUSPENSION,
+            expiry_next_step='2014-01-01')
         ex = expirer.PTExpirer(project)
         new_expiry = datetime.datetime.now() + relativedelta(days=30)
         new_expiry = new_expiry.strftime(expirer.DATE_FORMAT)
@@ -1269,7 +1271,7 @@ class PTExpiryTests(test.TestCase):
             mock_notification.assert_called_with('suspended')
             mock_event.assert_called_once_with('suspended')
             mock_update_resource.assert_called_with(
-                expiry_status='suspended',
+                expiry_status=expiry_states.SUSPENDED,
                 expiry_next_step=new_expiry)
             mock_archiver.zero_quota.assert_called_with()
             mock_archiver.stop_resources.assert_called_with()
