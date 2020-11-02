caa70b253c53b2f73d105a9e8b9f3148315b0c1f
Stephen Crawley <s.crawley@uq.edu.au>
2020-11-02 18:15:14 +1100

    Add tests for stuck zone and allocation expiry
    Change-Id: Iec32585905b67fbc3b36855dbb142c4ceb53f47e

--- nectar_tools/audit/projects/allocation.py
+++ nectar_tools/audit/projects/allocation.py
@@ -1,3 +1,5 @@
+import datetime
+from dateutil.relativedelta import relativedelta
 import logging
 
 from nectarallocationclient import exceptions as allocation_exceptions
@@ -10,12 +12,15 @@ from nectar_tools.expiry import expiry_states
 
 LOG = logging.getLogger(__name__)
 
+DATE_FORMAT = '%Y-%m-%d'
+
 
 class ProjectAllocationAuditor(base.ProjectAuditor):
 
     def setup_clients(self):
         super().setup_clients()
         self.a_client = auth.get_allocation_client(sess=self.ks_session)
+        self.now = datetime.datetime.now()
 
     def check_allocation_id(self):
         allocation_id = getattr(self.project, 'allocation_id', None)
@@ -58,16 +63,72 @@ class ProjectAllocationAuditor(base.ProjectAuditor):
             LOG.error("%s: Linked allocation (%s)'s project_id is wrong",
                       self.project.id, allocation_id)
 
-    def check_deleted_allocation(self):
+    def _past_next_step(self, date_string, days=3):
+        # Return True when 'date_string' is at least 3 days in the past.
+        # This allows for occasional expiry system glitches ...
+        if not date_string:
+            return False
+        date = datetime.datetime.strptime(date_string, DATE_FORMAT)
+        date_with_slack = date + relativedelta(days=days)
+        return date_with_slack < self.now
+
+    def _quietly_get_allocation(self):
         allocation_id = getattr(self.project, 'allocation_id', None)
-        expiry_status = getattr(self.project, 'expiry_status', '')
         if not allocation_id:
-            return
+            return None
         try:
-            allocation = self.a_client.allocations.get(allocation_id)
+            return self.a_client.allocations.get(allocation_id)
         except allocation_exceptions.NotFound:
-            # Reported by another check
+            return None
+
+    def check_expiry(self):
+        expiry_status = getattr(self.project, 'expiry_status', '')
+        if not expiry_status:
             return
+        expiry_next_step = getattr(self.project, 'expiry_next_step', None)
+        if self._past_next_step(expiry_next_step):
+            if expiry_status in ('warning', 'stopped', 'restricted',
+                                 'archiving', 'archived'):
+                allocation = self._quietly_get_allocation()
+                if allocation and allocation.status == 'X':
+                    LOG.info("%s: Allocation expiry blocked on approval "
+                             "decision in state %s",
+                             self.project.id, expiry_status)
+                elif allocation:
+                    LOG.error("%s: Allocation expiry stuck in alloc state %s, "
+                              "expiry state %s",
+                              self.project.id, allocation.status,
+                              expiry_status)
+                else:
+                    LOG.error("%s: Allocation expiry for missing allocation: "
+                              "expiry state %s",
+                              self.project.id, expiry_status)
+            elif expiry_status not in ('deleted', 'renewed', 'admin'):
+                LOG.warn("%s: Allocation expiry stuck in ticket hold: %s",
+                         self.project.id, expiry_status)
+
+    def check_out_of_zone_expiry(self):
+        zone_expiry_status = getattr(self.project, 'zone_expiry_status', '')
+        if not zone_expiry_status or \
+           not hasattr(self.project, 'compute_zones'):  # was reclassified ...
+            return
+        zone_expiry_next_step = getattr(
+            self.project, 'zone_expiry_next_step', None)
+        if self._past_next_step(zone_expiry_next_step):
+            if zone_expiry_status in ('warning', 'stopped',
+                                      'archiving', 'archived'):
+                LOG.error("%s: Instance out of zone expiry stuck in %s",
+                          self.project.id, zone_expiry_status)
+            else:
+                LOG.warn("%s: Instance out of zone expiry stuck in "
+                         "ticket hold: %s",
+                         self.project.id, zone_expiry_status)
+
+    def check_deleted_allocation(self):
+        allocation = self._quietly_get_allocation()
+        if not allocation:
+            return
+        expiry_status = getattr(self.project, 'expiry_status', '')
 
         # These are also reported by another check.  If the linked allocation
         # record has any of these problems, then we can't trust its allocation
