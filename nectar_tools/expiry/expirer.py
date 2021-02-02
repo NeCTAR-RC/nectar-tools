3dc831426810d70d1d29f05fff6b9e0301426bcf
Sam Morrison <sorrison@gmail.com>
2021-02-03 09:01:08 +1100

    PT expiry: Only check for pending allocations if other checks fail
    Change-Id: I14fc255a2ca2def8bdd67be7f5dafe342a1c77a8

--- nectar_tools/expiry/expirer.py
+++ nectar_tools/expiry/expirer.py
@@ -695,12 +695,17 @@ class PTExpirer(ProjectExpirer):
         personal = self.is_personal_project()
         if personal and not has_owner:
             LOG.warn("%s: Project has no owner", self.project.id)
+            return False
+        if not personal:
+            return False
+        if self.is_ignored_project():
+            return False
         allocations = self.pending_allocations()
         if allocations:
             LOG.warn("%s: Skipping expiry due to pending allocations %s",
                      self.project.id, [a.id for a in allocations])
             return False
-        return personal and has_owner and not self.is_ignored_project()
+        return True
 
     def is_personal_project(self):
         return PT_RE.match(self.project.name)
