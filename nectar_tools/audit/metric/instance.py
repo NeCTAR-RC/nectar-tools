17447f07d31317c5d48562137e9d955aa54899c5
Sam Morrison <sorrison@gmail.com>
2021-02-10 14:36:58 +1100

    New audit to ensure gnocchi instances marked as deleted
    Change-Id: I01b5aae56d6f52c8b944b68ba0706edb16110470

--- nectar_tools/audit/metric/instance.py
+++ nectar_tools/audit/metric/instance.py
@@ -87,3 +87,87 @@ class InstanceAuditor(base.ResourceAuditor):
                 LOG.error("%s: Nova instance has no AZ",
                           instance['id'])
                 continue
+
+    def ensure_marked_deleted(self):
+        self._ensure_marked_deleted('2010-01-01', '2015-01-01')
+        self._ensure_marked_deleted('2015-01-01', '2016-01-01')
+        self._ensure_marked_deleted('2016-01-01', '2017-01-01')
+        self._ensure_marked_deleted('2017-01-01', '2018-01-01')
+        self._ensure_marked_deleted('2018-01-01', '2019-01-01')
+        self._ensure_marked_deleted('2019-01-01', '2020-01-01')
+        self._ensure_marked_deleted('2020-01-01', '2020-02-01')
+        self._ensure_marked_deleted('2020-02-01', '2020-03-01')
+        self._ensure_marked_deleted('2020-03-01', '2020-04-01')
+        self._ensure_marked_deleted('2020-04-01', '2020-05-01')
+        self._ensure_marked_deleted('2020-05-01', '2020-06-01')
+        self._ensure_marked_deleted('2020-06-01', '2020-07-01')
+        self._ensure_marked_deleted('2020-07-01', '2020-08-01')
+        self._ensure_marked_deleted('2020-08-01', '2020-09-01')
+        self._ensure_marked_deleted('2020-09-01', '2020-10-01')
+        self._ensure_marked_deleted('2020-10-01', '2020-11-01')
+        self._ensure_marked_deleted('2020-11-01', '2020-12-01')
+        self._ensure_marked_deleted('2020-12-01', '2021-01-01')
+        self._ensure_marked_deleted('2021-01-01', '2021-02-01')
+        self._ensure_marked_deleted('2021-02-01', '2021-03-01')
+        self._ensure_marked_deleted('2021-03-01', '2021-04-01')
+        self._ensure_marked_deleted('2021-04-01', '2021-05-01')
+        self._ensure_marked_deleted('2021-05-01', '2021-06-01')
+
+    def _ensure_marked_deleted(self, start, end):
+        instances = self.g_client.resource.search(
+            resource_type='instance',
+            query="ended_at = none and started_at >= '%s' and started_at <= '%s'" % (start, end),
+            limit=3000,
+        )
+        LOG.info("Start %s, End %s", start, end)
+        LOG.info("Found %s instances", len(instances))
+
+        for instance in instances:
+            LOG.debug("Processing instance %s", instance['id'])
+            try:
+                nova_instance = self.n_client.servers.get(instance['id'])
+                LOG.debug("Found running instance")
+                continue
+            except novaclient.exceptions.NotFound:
+                opts = {'deleted': True,
+                        'all_tenants': True,
+                        'user_id': instance['user_id'],
+                        'project_id': instance['project_id'],
+                        'name': instance['display_name']}
+                        # 'host': instance['host']}
+                try:
+                    nova_instances = self.n_client.servers.list(
+                        search_opts=opts)
+                except Exception:
+                    LOG.error("Failed to list instances")
+                    continue
+
+                nova_instance = None
+                if nova_instances:
+                    for i in nova_instances:
+                        if i.id == instance['id']:
+                            nova_instance = i
+                            break
+                if nova_instance is None or not nova_instances:
+                    LOG.warn("Can't find deleted instance in nova %s",
+                             instance)
+                    continue
+            deleted_at = getattr(nova_instance, 'OS-SRV-USG:terminated_at',
+                                 None)
+            if deleted_at:
+                if self.repair:
+                    LOG.info("%s: Setting ended_at, host=%s, AZ=%s",
+                             instance['id'], instance['availability_zone'],
+                             instance['host'])
+                    self.g_client.resource.update(
+                        'instance', instance['id'],
+                        {'started_at': nova_instance.created})
+                    self.g_client.resource.update(
+                        'instance', instance['id'],
+                        {'ended_at': deleted_at})
+                else:
+                    LOG.info("%s: Would set ended_at", instance['id'])
+            else:
+                LOG.error("%s: Nova instance has no deleted_at",
+                          instance['id'])
+                continue
