e6cf37daf84726f2c355c73fe27680c5c7491d87
Sam Morrison <sorrison@gmail.com>
2021-02-15 10:06:51 +1100

    New audit to delete murano envs from deleted projects
    Change-Id: I8d7e1bb084c91ce8a29adbe5a184180324b42ec0

--- nectar_tools/audit/app_catalog/environment.py
+++ nectar_tools/audit/app_catalog/environment.py
@@ -17,6 +17,7 @@ class EnvironmentAuditor(base.Auditor):
     def setup_clients(self):
         super().setup_clients()
         self.mc = auth.get_murano_client(sess=self.ks_session)
+        self.k_client = auth.get_keystone_client(sess=self.ks_session)
 
     def check_environment_states(self):
         for env in self.mc.environments.list(all_tenants=True):
@@ -44,3 +45,17 @@ class EnvironmentAuditor(base.Auditor):
                     LOG.info('environment %s in state %s for more '
                              'than %s days (packages: %s)\n',
                              env.id, env.status, DAYS, ','.join(packages))
+
+    def check_environment_deleted_project(self):
+        for env in self.mc.environments.list(all_tenants=True):
+            project = self.k_client.projects.get(env.tenant_id)
+            if not project.enabled or getattr(
+                    project, 'expiry_status', 'active') == 'deleted':
+                LOG.error("Environment %s belongs to a deleted project %s",
+                          env.id, project.id)
+                if self.repair:
+                    try:
+                        self.mc.environments.delete(env.id, abandon=True)
+                        LOG.info("Removed environment %s", env.id)
+                    except Exception:
+                        pass
