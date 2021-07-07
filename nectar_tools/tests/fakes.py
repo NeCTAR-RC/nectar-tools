6516078aec793606f91484c85230c158ff66c732
Sam Morrison <sorrison@gmail.com>
2021-07-07 14:16:07 +1000

    Expire murano resources for PTs too
    Change-Id: I4a209407cee6c5d1810789d60a96994f211587c9

--- nectar_tools/tests/fakes.py
+++ nectar_tools/tests/fakes.py
@@ -240,6 +240,19 @@ class FakeStack(object):
         return getattr(self, key, default)
 
 
+class FakeEnvironment(object):
+
+    def __init__(self, id='fake', stack_status='CREATE_COMPLETE',
+                 **kwargs):
+        self.id = id
+        self.stack_status = stack_status
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+
+
 COMPUTE_HOMES = {'auckland': ['auckland'],
                 'ersa': ['sa'],
                 'intersect': ['intersect'],
