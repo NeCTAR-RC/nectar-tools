1cce51f21957ee70a17f5714d9af2eb609efad21
Sam Morrison <sorrison@gmail.com>
2021-07-12 13:07:01 +1000

    WIP: floating IP auditor
    Change-Id: I31fdfefd364c3131a23e1234eaeab837ecd39850

---
+++ nectar_tools/audit/network/floating_ip.py
@@ -0,0 +1,46 @@
+import logging
+
+from nectar_tools.audit.metric import base
+from nectar_tools import auth
+
+import nectar_tools.audit.network.siteLookUpTable as lookUpTable
+
+LOG = logging.getLogger(__name__)
+
+
+#This audit finds floating networks that are attached to an instance on a different site
+class FloatingIPAuditor(base.ResourceAuditor):
+
+    def setup_clients(self):
+        super().setup_clients()
+        self.nc = auth.get_neutron_client(sess=self.ks_session)
+
+    def check_availability_zone(self):
+        floating_ips = self.nc.list_floatingips()
+        for floating_ip in floating_ips['floatingips']:
+            port_id = floating_ip["port_id"]
+
+            #if there is no port id, this floating ip can't be attached to an instance
+            if port_id != None:
+                port = self.nc.show_port(port_id)
+                device_owner = port['port']['device_owner']
+
+                #We are looking for device_owner string that looks like compute:AZ
+                if device_owner != None:
+                    device_owner_name = device_owner.split(':')[0]
+                    if device_owner_name == "compute":
+                        device_id = port['port']['device_id']
+                        device_owner_az = device_owner.split(':')[1]
+
+                        floating_ip_id=floating_ip['id']
+                        network_name=self.nc.show_network(floating_ip['floating_network_id'])['network']['name'] #noqa
+
+                        #Translate Network name and AZ into sites
+                        networkSite = lookUpTable.NetworkToSite.get(network_name)
+                        AZSite = lookUpTable.AZToSite.get(device_owner_az)
+
+                        if network_name == None or AZSite == None or networkSite != AZSite:
+                            LOG.error("floating_ip_id %s network_name %s network_site %s instance_id %s instance_AZ %s instance_site %s" % (floating_ip_id, network_name, networkSite, device_id, device_owner_az, AZSite )) #noqa
+
+
+
---
+++ nectar_tools/audit/network/siteLookUpTable.py
@@ -0,0 +1,61 @@
+#lookup table from network and AZ to Site
+#
+#Used:
+#o floating ip list --c 'Floating Network' -f value | sort | uniq | xargs -L1 openstack network show -c name -f value
+#to get the networks (this will only list the floating networks)
+#
+#Used:
+#gnocchi resource list --type site -c name -f value| sort | uniq
+#to get the sites
+#
+#Used:
+#o availability zone list -c 'Zone Name' -f value | sort | uniq
+#to get the availability zones
+#
+
+NetworkToSite = {'public': 'luna', #dev
+                'coreservices': 'coreservices', #rctest
+                'qh2-test-floating': 'melbourne',
+                'QRIScloud': 'QRIScloud' , #nectar
+                'tasmania': 'tasmania',
+                'qld': 'intersect',
+                'swinburne': 'swinburne' ,
+                'auckland': 'auckland',
+                'qh2-uom': 'melbourne',
+                'melbourne': 'melbourne',
+                'qh2-uom-internal': 'melbourne',
+                'monash-02': 'monash',
+                'monash-01': 'monash',
+                'monash': 'monash'
+                }
+
+AZToSite = {'lani': 'lani', #dev
+            'luna': 'luna',
+            'bogus': 'unknown',
+            'coreservices': 'coreservices', #rctest
+            'qh2-test-floating': 'melbourne',
+            'auckland':'auckland',#nectar
+            'intersect':'intersect',
+            'melbourne-qh2':'melbourne',
+            'melbourne-qh2-uom':'melbourne',
+            'monash-01':'monash',
+            'monash-02':'monash',
+            'monash-03':'monash',
+            'QRIScloud':'QRIScloud',
+            'swinburne-01':'swinburne',
+            'tasmania':'tasmania',
+            'tasmania-02':'tasmania',
+            'tasmania-s':'tasmania',
+            'internal':'unknown', #shared
+            'nova': 'unknown'
+            }
+
+
+
+
+
+
+
+
+
+
