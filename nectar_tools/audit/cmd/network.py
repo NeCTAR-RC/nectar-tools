8736806256556fd961fcf1d5ff7275bfd85b425d
Sam Morrison <sorrison@gmail.com>
2021-07-12 13:07:01 +1000

    WIP: floating IP auditor
    Change-Id: I31fdfefd364c3131a23e1234eaeab837ecd39850

---
+++ nectar_tools/audit/cmd/network.py
@@ -0,0 +1,20 @@
+from nectar_tools.audit.cmd import base
+from nectar_tools.audit.network import floating_ip
+
+
+class NetworkAuditorCmd(base.AuditCmdBase):
+
+    AUDITORS = [floating_ip.FloatingIPAuditor]
+
+    def add_args(self):
+        super(NetworkAuditorCmd, self).add_args()
+        self.parser.description = 'Network auditor'
+
+
+def main():
+    cmd = NetworkAuditorCmd()
+    cmd.run_audits()
+
+
+if __name__ == '__main__':
+    main()
