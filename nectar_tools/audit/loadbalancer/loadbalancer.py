ec325039eb3f7ff4f552d41e82c424e18ce1dc5a
Sam Morrison <sorrison@gmail.com>
2021-07-29 14:50:37 +1000

    Add new audit for LB amphora using old images
    Change-Id: I8267c1661d82227de20f8e2c469299d928c83f0a

--- nectar_tools/audit/loadbalancer/loadbalancer.py
+++ nectar_tools/audit/loadbalancer/loadbalancer.py
@@ -17,6 +17,7 @@ class LoadBalancerAuditor(base.Auditor):
         super().setup_clients()
         self.openstack = auth.get_openstacksdk(sess=self.ks_session)
         self.n_client = auth.get_nova_client(sess=self.ks_session)
+        self.g_client = auth.get_glance_client(sess=self.ks_session)
 
     def check_nova_instances(self):
         instances = self.n_client.servers.list(
@@ -34,3 +35,31 @@ class LoadBalancerAuditor(base.Auditor):
                          instance.id)
                 self.repair(f"Deleting orphaned amp instance {instance.id}",
                             lambda: instance.delete())
+
+    def old_amphora_image(self):
+        images = self.g_client.images.list(filters={'tag': ['octavia']})
+        latest_image = list(images)[0]
+
+        LOG.debug(f"Latest image ID is {latest_image.id}")
+
+        lbs = self.openstack.load_balancer.load_balancers()
+        count = 1
+        for lb in lbs:
+            if lb.provisioning_status != 'ACTIVE':
+                LOG.warning(f"LB {lb.id} in state {lb.provisioning_status}")
+                continue
+
+            amphorae = self.openstack.load_balancer.amphorae(
+                loadbalancer_id=lb.id)
+            for amp in amphorae:
+                if amp.get('image_id') != latest_image.id:
+                    LOG.warn(f"LB {lb.id} not using latest image")
+                    self.repair(
+                        f"Fail over LB {lb.id}", lambda:
+                        self.openstack.load_balancer.failover_load_balancer(
+                            lb.id)
+                        )
+                    count += 1
+                    break
+                else:
+                    LOG.debug(f"LB {lb.id} using latest image")
