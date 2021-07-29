3454efe85340ac5831954214e240dac7f73daa61
Sam Morrison <sorrison@gmail.com>
2021-07-29 14:48:43 +1000

    Add a --limit argument to audits
    Change-Id: I4a917999ed41b74f2e1ab131eaa85b00abcbe13a

--- nectar_tools/audit/cmd/base.py
+++ nectar_tools/audit/cmd/base.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 from nectar_tools import cmd_base
+from nectar_tools import exceptions
 
 
 LOG = logging.getLogger(__name__)
@@ -14,6 +15,7 @@ class AuditCmdBase(cmd_base.CmdBase):
         super(AuditCmdBase, self).__init__(log_filename='audit.log')
 
         self.list_not_run = self.args.list
+        self.limit = self.args.limit
 
         if self.args.check:
             try:
@@ -22,11 +24,15 @@ class AuditCmdBase(cmd_base.CmdBase):
                 module = importlib.import_module(module_str)
                 auditor_class = getattr(module, class_str)
                 auditor = auditor_class(ks_session=self.session,
-                                        dry_run=self.dry_run)
+                                        dry_run=self.dry_run,
+                                        limit=self.limit)
                 method = getattr(auditor, method_str)
                 method()
                 summary = getattr(auditor, 'summary')
                 summary()
+                sys.exit(0)
+            except exceptions.LimitReached:
+                LOG.info("Limit has been reached")
                 sys.exit(0)
             except Exception as e:
                 LOG.exception(e)
@@ -35,12 +41,17 @@ class AuditCmdBase(cmd_base.CmdBase):
     def run_audits(self, **kwargs):
         for auditor in self.AUDITORS:
             a = auditor(ks_session=self.session,
-                        dry_run=self.dry_run)
+                        dry_run=self.dry_run,
+                        limit=self.limit)
             a.run_all(list_not_run=self.list_not_run, **kwargs)
 
     def add_args(self):
         super(AuditCmdBase, self).add_args()
         self.parser.add_argument('-l', '--list', action='store_true',
                                  help="List audits but don't run them")
+        self.parser.add_argument('--limit',
+                            type=int,
+                            default=0,
+                            help='Only process this many eligible items.')
         self.parser.add_argument('check', nargs='?',
                                  help="specific check to run")
