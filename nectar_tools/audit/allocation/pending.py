d02baa10f8ca76650789f659447ff751517ea253
Stephen Crawley <s.crawley@uq.edu.au>
2022-09-14 03:47:45 +0000

    Use APIs urgency & expiry information
    Previously we computed the urgency info by copying the code (and
    tables) used by allocations.  Now we get it by a new API call.  We
    also get the (real) expiry state from Keystone.
    
    Change-Id: I2d0e8cb389545bb13961f6d6a4028fe4ec5ab3a3

--- nectar_tools/audit/allocation/pending.py
+++ nectar_tools/audit/allocation/pending.py
@@ -1,10 +1,10 @@
-from datetime import date
-from datetime import timedelta
 import logging
 
-from dateutil import parser
+import keystoneauth1
 
 from nectar_tools.audit.allocation import base
+from nectar_tools import auth
+from nectar_tools.expiry import expiry_states
 
 
 LOG = logging.getLogger(__name__)
@@ -12,41 +12,42 @@ LOG = logging.getLogger(__name__)
 
 class PendingAllocationAuditor(base.AllocationAuditorBase):
 
+    def setup_clients(self):
+        super().setup_clients()
+        self.k_client = auth.get_keystone_client(sess=self.ks_session)
+
     def check_pending(self, allocation_id=None):
-        today = date.today()
         allocations = self._get_allocations(allocation_id, pending=True)
         for a in allocations:
-            # This roughly corresponds to the 'urgency' classification
-            # in the Pending Allocations page.  One difference is that
-            # we are separating the expiry and age criteria.
-            mod_date = parser.parse(a.modified_time).date()
-            if a.end_date:
-                end_date = parser.parse(a.end_date).date()
-                if end_date < today:
-                    if mod_date + timedelta(days=30 * 5) < today:
-                        expiry = "Danger"
-                    elif end_date + timedelta(days=28) < today:
-                        expiry = "Archived"
-                    elif end_date + timedelta(days=14) < today:
-                        expiry = "Stopped"
-                    else:
-                        expiry = "Expiring"
+            approver_info = a.get_approver_info()
+            if a.project_id:
+                try:
+                    p = self.k_client.projects.get(a.project_id)
+                    expiry_status = getattr(p, 'expiry_status', '')
+                except keystoneauth1.exceptions.http.NotFound:
+                    LOG.warning("Allocation %s: allocation's project (%s) "
+                                "is missing",
+                                a.id, a.project_id)
+                    expiry_status = "Unknown (missing project!)"
+            else:
+                expiry_status = "N/A"
+            for site in approver_info['concerned_sites']:
+                if (approver_info['expiry_state'] in (
+                        "Danger", "Archived", "Stopped", "Expired")
+                    or approver_info["approval_urgency"] in (
+                        "Warning", "Overdue")
+                    or expiry_status in (expiry_states.STOPPED,
+                                         expiry_states.RESTRICTED,
+                                         expiry_states.ARCHIVING,
+                                         expiry_states.ARCHIVED)):
+                    level = logging.WARN
                 else:
-                    expiry = "Not expiring"
-            else:
-                expiry = "Not provisioned"
-
-            if mod_date + timedelta(days=21) < today:
-                urgency = "Overdue"
-            elif mod_date + timedelta(days=14) < today:
-                urgency = "Warning"
-            elif mod_date + timedelta(days=7) < today:
-                urgency = "Attention"
-            else:
-                urgency = "New"
-            if expiry in ("Danger", "Archived", "Stopped") \
-               or urgency in ("Warning", "Overdue"):
-                LOG.warning("Allocation %s: pending in status %s, "
-                            "expiry state '%s', urgency '%s', "
-                            "last mod date %s",
-                            a.id, a.status, expiry, urgency, mod_date)
+                    level = logging.INFO
+                LOG.log(level,
+                        "Allocation %s: pending in status %s, urgency '%s', "
+                        "last update %s, real expiry status %s, "
+                        "inferred expiry state %s",
+                        a.id, a.status, approver_info['approval_urgency'],
+                        a.modified_time, expiry_status,
+                        approver_info['expiry_state'],
+                        extra={'extra': {'site': site}})
