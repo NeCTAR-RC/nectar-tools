06f7ccb6101aad8347ac6aa589be10421a9fc0e1
Sam Morrison <sorrison@gmail.com>
2024-04-09 12:53:06 +1000

    Audit: Automatically fix up missing cinder_pool sites
    Change-Id: Icf8bb1fc4d41f4997abac060df280b8681d5f508

--- nectar_tools/audit/metric/cinder_pool.py
+++ nectar_tools/audit/metric/cinder_pool.py
@@ -1,6 +1,6 @@
 import logging
 
-
+from nectar_tools.audit import common
 from nectar_tools.audit.metric import base
 
 
@@ -15,10 +15,21 @@ class CinderPoolAuditor(base.ResourceAuditor):
             query='site=null')
 
         for cinder_pool in resources:
-            LOG.error("Cinder Pool %s has no site",
-                      cinder_pool['original_resource_id'])
-            LOG.info(cinder_pool['availability_zone'])
-            LOG.info("To fix with: "
-                     "gnocchi resource update "
-                     "--type cinder_pool "
-                     "-a 'site:<site>' %s", cinder_pool['id'])
+            az = cinder_pool['availability_zone']
+            LOG.error("Cinder Pool %s with az %s has no site",
+                      cinder_pool['original_resource_id'],
+                      az)
+            site = common.AZ_SITE_MAP.get(az)
+            id = cinder_pool['id']
+            if site:
+                self.repair(f"Setting site for {id} and az={az} "
+                            f"to {site}",
+                            lambda: self.g_client.resource.update(
+                                resource_type='cinder_pool',
+                                resource_id=id,
+                                resource={'site': site}))
+            else:
+                LOG.info("To fix with: "
+                         "gnocchi resource update "
+                         "--type cinder_pool "
+                         "-a 'site:<site>' %s", cinder_pool['id'])
