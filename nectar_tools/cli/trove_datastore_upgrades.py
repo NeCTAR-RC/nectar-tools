8d71b097478bf638c4fb5d8c281f1865b34c6493
Sam Morrison <sorrison@gmail.com>
2024-04-15 16:38:50 +1000

    Add trove database backup/upgrade action
    Change-Id: I75e9fed63cd48b133d99eb61a48ef6afff21c2cd

--- nectar_tools/cli/trove_datastore_upgrades.py
+++ nectar_tools/cli/trove_datastore_upgrades.py
@@ -1,3 +1,9 @@
+import datetime
+import sys
+import time
+
+from troveclient.apiclient import exceptions as t_exc
+
 from nectar_tools import auth
 from nectar_tools import cmd_base
 from nectar_tools import config
@@ -6,6 +12,22 @@ from nectar_tools import utils
 
 
 CONF = config.CONFIG
+
+
+class ActionError(Exception):
+    pass
+
+
+class BackupError(ActionError):
+    pass
+
+
+class UpgradeError(ActionError):
+    pass
+
+
+class UpgradeUnavailableError(ActionError):
+    pass
 
 
 class TroveDatastoreUpgradesCmd(cmd_base.CmdBase):
@@ -20,52 +42,175 @@ class TroveDatastoreUpgradesCmd(cmd_base.CmdBase):
             template_dir='databases',
             subject="Nectar Research Cloud Database Service",
             dry_run=not self.args.no_dry_run)
+        self.limit = self.args.limit
+        self.action = self.args.action.lower()
+        if self.action not in ['notify', 'upgrade']:
+            print("Acion not valid, choices upgrade,notify")
+            sys.exit(1)
+        if self.action == 'notify':
+            if not self.args.start or not self.args.end:
+                print("--start and --end are required with notify")
+                sys.exit(1)
 
     def add_args(self):
         """Handle command-line options"""
         super().add_args()
         self.parser.add_argument('--start',
-                                 help='Start date/time of maintenance',
-                                 required=True)
+                                 help='Start date/time of maintenance'),
         self.parser.add_argument('--end',
-                                 help='End date/time of maintenance',
-                                 required=True)
+                                 help='End date/time of maintenance'),
+        self.parser.add_argument('--instance',
+                                 help='Only act on certain instance')
         self.parser.add_argument('--datastore',
-                                 help='Only act on certain datastore')
+                                 help='Only act on certain datastore type')
+        self.parser.add_argument('--action',
+                                 default='notify',
+                                 help='Action to perform')
+        self.parser.add_argument('-l', '--limit',
+                                 type=int,
+                                 default=0,
+                                 help='Only process this many eligible '
+                                 'instances.')
 
     def run(self):
+        count = 0
         default_datastores = {}
         datastores = self.t_client.datastores.list()
         for ds in datastores:
             default_datastores[ds.name] = ds.default_version
-        for inst in self.t_client.mgmt_instances.list():
+        if self.args.instance:
+            instances = [self.t_client.instances.get(self.args.instance)]
+        else:
+            instances = self.t_client.mgmt_instances.list()
+        for inst in instances:
             ds_type = inst.datastore.get('type')
-            if ds_type.lower() != self.args.datastore.lower():
+            if (self.args.datastore
+                    and ds_type.lower() != self.args.datastore.lower()):
                 continue
             ds_version = inst.datastore.get('version')
             datastore_version = self.t_client.datastore_versions.get(
                 ds_type, ds_version)
             if default_datastores[ds_type] != datastore_version.id:
+                if self.limit and count >= self.limit:
+                    print("Limit reached")
+                    break
                 ds_latest_version = self.t_client.datastore_versions.get(
                     ds_type, default_datastores[ds_type])
                 print(f"Outdated datastore, {inst.id} "
                       f"running {ds_type} {ds_version}")
-                self.notifier.resource = inst
-                stage = 'warning-%s-%s' % (ds_type.lower(),
-                                           ds_version.split('-')[0])
-                project = self.k_client.projects.get(inst.tenant_id)
-                context = {
-                    'datastore_latest_version': ds_latest_version,
-                    'project': project,
-                    'start': self.args.start,
-                    'end': self.args.end}
-                recipient, cc = utils.get_project_recipients(self.k_client,
-                                                             project)
-                message = self.notifier.send_message(stage, recipient,
-                                                     extra_recipients=cc,
-                                                     extra_context=context
-                                                     )
-                print(message)
+                if self.action == 'notify':
+                    self.notify(inst, ds_type, ds_version, ds_latest_version)
+                elif self.action == 'upgrade':
+                    try:
+                        self.upgrade(inst, ds_latest_version)
+                    except UpgradeError as e:
+                        print(e)
+                        sys.exit(1)
+                    except BackupError as e:
+                        print(e)
+                    except UpgradeUnavailableError as e:
+                        print(e)
+                    else:
+                        count += 1
+
+    def notify(self, inst, ds_type, ds_version, ds_latest_version, project):
+        self.notifier.resource = inst
+        stage = 'warning-%s-%s' % (ds_type.lower(),
+                                   ds_version.split('-')[0])
+        project = self.k_client.projects.get(inst.tenant_id)
+        context = {
+            'datastore_latest_version': ds_latest_version,
+            'project': project,
+            'start': self.args.start,
+            'end': self.args.end}
+        recipient, cc = utils.get_project_recipients(self.k_client,
+                                                     project)
+        message = self.notifier.send_message(stage, recipient,
+                                             extra_recipients=cc,
+                                             extra_context=context
+                                             )
+        print(message)
+
+    def upgrade(self, inst, ds_latest_version):
+        if inst.datastore.get('type').lower() == 'postgresql':
+            current_major = inst.datastore.get('version').split('-')[0]
+            new_major = ds_latest_version.name.split('-')[0]
+            if current_major != new_major:
+                raise UpgradeUnavailableError(
+                    f"Inst: {inst.id} Can't major upgrade postgresql")
+        if inst.status not in ['ACTIVE', 'HEALTHY']:
+            raise UpgradeUnavailableError(f"Inst in {inst.status}")
+        try:
+            self.t_client.databases.list(inst)
+        except t_exc.BadRequest:
+            print(f"Instance {inst.id} RPC communication error")
+            return
+
+        backups = self.t_client.backups.list(instance_id=inst.id)
+        needs_backup = False
+        for b in backups:
+            if b.name == 'Pre Upgrade':
+                if b.status == 'BUILDING':
+                    raise BackupError("Backup Building")
+                if b.status == 'COMPLETED':
+                    created = datetime.datetime.strptime(
+                        b.created, "%Y-%m-%dT%H:%M:%S")
+                    now = datetime.datetime.utcnow()
+                    diff = (now - created).total_seconds()
+                    if diff < 36000:
+                        needs_backup = False
+                        break
+
+        if needs_backup:
+            print(f"Backing up {inst.id}")
+            backup = self.t_client.backups.create(
+                name="Pre Upgrade", instance=inst,
+                description="Admin inititated backup pre upgrade")
+            start_time = int(time.time())
+            timeout = 1800
+            interval = 5
+            while True:
+                backup = self.t_client.backups.get(backup.id)
+                if backup.status == 'COMPLETED':
+                    print(f"Backup for instance {inst.id} created. "
+                          f"ID={backup.id}")
+                    break
+                elif backup.status in ['ERROR', 'FAILED']:
+                    raise BackupError(f"Backup {backup.id} failed")
+                if int(time.time()) - start_time >= timeout:
+                    raise BackupError(
+                        f"Timed out waiting for backup {backup.id}")
+
+                print(
+                    f"Backup {backup.id} in status {backup.status}, retrying")
+                time.sleep(interval)
+        else:
+            print(f"Instance {inst.id}: Skipping backup, have recent")
+
+        print(f"Upgrading {inst.id}")
+        try:
+            self.t_client.instances.upgrade(inst, ds_latest_version.id)
+        except Exception as e:
+            print(e)
+            return
+
+        start_time = int(time.time())
+        timeout = 1200
+        interval = 10
+        while True:
+            inst = self.t_client.instances.get(inst.id)
+            if inst.status in ['ACTIVE', 'HEALTHY']:
+                print(f"Inst {inst.id} Upgrade complete")
+                break
+            elif inst.status == 'ERROR':
+                raise UpgradeError(
+                    f"Inst {inst.id} upgrade failed, instance in ERROR")
+            if int(time.time()) - start_time >= timeout:
+                raise UpgradeError(
+                    f"Timed out waiting for upgrade inst {inst.id}")
+
+            print(f"Inst {inst.id} in status {inst.status}, retrying")
+            time.sleep(interval)
 
 
 def main():
