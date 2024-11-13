15ec8b7c945861b5bf681121a58e5a8b8a722bba
Dylan McCulloch <dylan.mcculloch@gmail.com>
2024-11-13 16:03:52 +1100

    Update warre-host-flavors script as some hosts serve multiple flavors
    Change-Id: I1c255a4dfdf060206395a947677c9ab05e44aeb1

--- nectar_tools/cli/warre_host_flavors.py
+++ nectar_tools/cli/warre_host_flavors.py
@@ -23,30 +23,37 @@ class WarreHostFlavors:
         except Exception:
             return None
 
-    def get_flavor_from_warre_id(self, flavors, warre_id):
+    def get_flavors_from_warre_id(self, flavors, warre_id):
         warre_id_regex = re.escape(warre_id)
+        warre_id_flavors = []
         for flavor in flavors:
-            try:
-                if re.search(warre_id_regex, flavor.properties):
-                    return flavor
-            except Exception:
-                pass
+            if flavor.active:
+                try:
+                    if re.search(warre_id_regex, flavor.properties):
+                        warre_id_flavors.append(flavor)
+                except Exception:
+                    pass
+        return warre_id_flavors
 
     def add_hosts_flavors_to_table(self, hosts, flavors, table):
         for host in hosts:
-            flavor_name = None
+            flavor_names = []
             warre_id = self.get_host_warre_id(host)
             if warre_id:
-                flavor = self.get_flavor_from_warre_id(flavors, warre_id)
-                try:
-                    flavor_name = flavor.name
-                except Exception:
-                    flavor_name = None
-                try:
-                    host_name = host['hypervisor_hostname']
-                except Exception:
-                    host_name = None
-                table.add_row([host_name, warre_id, flavor_name])
+                warre_id_flavors = self.get_flavors_from_warre_id(
+                    flavors, warre_id
+                )
+                for flavor in warre_id_flavors:
+                    try:
+                        flavor_name = flavor.name
+                        flavor_names.append(flavor_name)
+                    except Exception:
+                        flavor_name = None
+                    try:
+                        host_name = host['hypervisor_hostname']
+                    except Exception:
+                        host_name = None
+                    table.add_row([host_name, warre_id, flavor_names])
         return table
 
 
